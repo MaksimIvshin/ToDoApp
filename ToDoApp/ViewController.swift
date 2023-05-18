d2022d16ab1e6e26695b15b630ea14cd99ce5328
MaksimIvshin <mi375293086861@gmail.com>
2023-05-18 20:49:51 +0300

    ToDoAppProject

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Maks Ivshin on 18.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
